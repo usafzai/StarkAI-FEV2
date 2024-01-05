818f6d98ce797be1f652d7d9c0bf935cfa55b885
usafzai <miyamotohiro.contact@gmail.com>
2024-01-05 10:53:03 -0500

    add social links

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,11 +1,31 @@
+import { Icon } from "@iconify/react";
+
 const Header = () => {
   return (
     <div className="px-4 md:px-6 lg:px-14 xl:px-[calc((100vw-var(--max-width))/2)] fixed top-0 bg-darkBackground z-20 w-full">
-      <div className="w-full flex justify-between items-center border-b border-white border-opacity-10 h-12 gap-5">
-        <div className="">
-          <img src="./favicon.ico" alt="logo" className="w-[58px] h-[18px]" />
+      <div className="w-full flex justify-between items-center border-b border-white border-opacity-10 h-12 gap-5 px-10">
+        <span className="">
+          <h2 className="text-white font-semibold text-[32px]">Spark AI</h2>
+        </span>
+        <div className="flex flex-row gap-8">
+          <span>
+            <Icon
+              icon="ri:twitter-x-fill"
+              className="text-white"
+              width="22"
+              height="22"
+            />
+          </span>
+          <span>
+            <Icon
+              icon="ic:round-discord"
+              className="text-white dark:text-darkIconColor"
+              width="22"
+              height="22"
+            />
+          </span>
+          <span></span>
         </div>
-        <div className=""></div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      darkBackground: "#0D0D0D",
+    extend: {
+      colors: {
+        darkBackground: "#0D0D0D",
+      },
     },
   },
   plugins: [],
