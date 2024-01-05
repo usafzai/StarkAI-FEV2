847111c8483fd91bf9eb1cf9fb90e425f253eda0
usafzai <miyamotohiro.contact@gmail.com>
2024-01-05 08:58:15 -0500

    update login

--- src/layouts/login.tsx
+++ src/layouts/login.tsx
@@ -10,20 +10,28 @@ const Login = () => {
     return <Navigate to="/" />;
   }
   return (
-    <>
-      <GoogleLogin
-        onSuccess={(credentialResponse) => {
-          if (!credentialResponse.credential) return;
-          let data: { email: string } = jwtDecode(
-            credentialResponse.credential
-          );
-          setUser(JSON.stringify(data));
-        }}
-        onError={() => {
-          console.log("Login Failed");
-        }}
-      />
-    </>
+    <div className="flex bg-black justify-center">
+      <div className="flex flex-col gap-4 text-white pt-36">
+        <div className="text-2xl font-bold">READY TO USE STARKMETA?</div>
+        <div className="text-xl">Sign in</div>
+        <div className="w-full">
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              if (!credentialResponse.credential) return;
+              let data: { email: string } = jwtDecode(
+                credentialResponse.credential
+              );
+              setUser(JSON.stringify(data));
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+            size="large"
+            width={360}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
