93c87d63b1f511305f7aef060adb4a3788b32575
usafzai <miyamotohiro.contact@gmail.com>
2024-01-08 08:21:03 -0500

    update dashboard layout

--- src/layouts/LayoutDashboard.tsx
+++ src/layouts/LayoutDashboard.tsx
@@ -6,10 +6,10 @@ const LayoutDashboard = () => {
       <div className="w-full max-w-[1400px] mx-auto flex flex-row h-full">
         <div className="w-1/2 h-full pr-5">
           <div className="items-center justify-center flex flex-col dashboard-bg gap-2">
-            <span className="px-5 w-full h-[200px] bg-gray-600/30 text-white text-center font-semibold text-[24px] z-10 flex items-center">
+            <span className="px-5 w-full h-[200px] text-white text-center font-semibold text-[24px] z-10 flex items-center">
               A canvas of Infinite creativity for unique creators
             </span>
-            <span className="bg-black/30 z-[5] mt-[-30px] p-10 flex flex-col justify-center items-center gap-3 rounded-md">
+            <span className="z-[5] mt-[-30px] p-10 flex flex-col justify-center items-center gap-3 rounded-md">
               <Link to="/login" className="primary-button z-10">
                 Create Account
               </Link>
