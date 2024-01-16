b634274bfa4c94b10958600a381f08ad7473faf9
usafzai <miyamotohiro.contact@gmail.com>
2024-01-16 08:42:54 -0500

    update layouts

---
+++ src/components/Settings/index.tsx
@@ -0,0 +1,13 @@
+const Settings = () => {
+  return (
+    <>
+      <div className="w-full bg-black h-screen pt-[29px] flex flex-col">
+        <div className="pl-8">
+          <span className="font-chakra text-[26px]">Settings</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Settings;
--- src/layouts/App/AppLayout.tsx
+++ src/layouts/App/AppLayout.tsx
@@ -4,6 +4,10 @@ import CommunityFeed from "../../components/CommunityFeed";
 import PersonalFeed from "../../components/PersonalFeed";
 import AppHome from "../../components/AppHome";
 import ImageGeneration from "../../components/ImageGeneration";
+import Explore from "../../components/Explore";
+import Marketplace from "../../components/Marketplace";
+import TextGeneration from "../../components/TextGeneration";
+import Settings from "../../components/Settings";
 
 const AppLayout = () => {
   return (
@@ -14,8 +18,10 @@ const AppLayout = () => {
         <Route path="community-feed" element={<CommunityFeed />} />
         <Route path="personal-feed" element={<PersonalFeed />} />
         <Route path="image-generator" element={<ImageGeneration />} />
-        <Route path="text-generator" element={<CommunityFeed />} />
-        <Route path="settings" element={<CommunityFeed />} />
+        <Route path="text-generator" element={<TextGeneration />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="explore" element={<Explore />} />
+        <Route path="marketplace" element={<Marketplace />} />
       </Routes>
     </div>
   );
