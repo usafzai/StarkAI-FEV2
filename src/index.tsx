b48180257f71c21c0653e1c41a15c79916604904
usafzai <miyamotohiro.contact@gmail.com>
2024-01-25 20:53:05 -0500

    update chains

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import {
   darkTheme,
 } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
+import { mainnet, polygon, bsc, goerli, bscTestnet } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
@@ -19,7 +19,7 @@ import "@rainbow-me/rainbowkit/styles.css";
 import "./index.css";
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, base, zora],
+  [mainnet, goerli, bsc, bscTestnet, polygon],
   [
     alchemyProvider({
       apiKey:
