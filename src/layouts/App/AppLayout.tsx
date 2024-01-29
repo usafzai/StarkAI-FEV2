cbc9ad9ec7f0590dfe5b1743f1b1133b788bdd8b
usafzai <miyamotohiro.contact@gmail.com>
2024-01-29 17:26:32 -0500

    add footer info section

--- src/layouts/App/AppLayout.tsx
+++ src/layouts/App/AppLayout.tsx
@@ -9,24 +9,28 @@ import Marketplace from "../../components/Marketplace";
 import TextGeneration from "../../components/TextGeneration";
 import Settings from "../../components/Settings";
 import ChainHeader from "../../components/Others/ChainHeader";
+import AppFooter from "./AppFooter";
 
 const AppLayout = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full bg-black">
-      <AppSlider />
-      <div className="flex flex-col min-h-screen h-full bg-black w-full">
-        <ChainHeader />
-        <Routes>
-          <Route path="/" element={<AppHome />} />
-          <Route path="community-feed" element={<CommunityFeed />} />
-          <Route path="personal-feed" element={<PersonalFeed />} />
-          <Route path="image-generator" element={<ImageGeneration />} />
-          <Route path="text-generator" element={<TextGeneration />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="explore" element={<Explore />} />
-          <Route path="marketplace" element={<Marketplace />} />
-        </Routes>
+    <div className="flex flex-col w-full">
+      <div className="flex flex-col md:flex-row w-full bg-black">
+        <AppSlider />
+        <div className="flex flex-col min-h-screen h-full bg-black w-full">
+          <ChainHeader />
+          <Routes>
+            <Route path="/" element={<AppHome />} />
+            <Route path="community-feed" element={<CommunityFeed />} />
+            <Route path="personal-feed" element={<PersonalFeed />} />
+            <Route path="image-generator" element={<ImageGeneration />} />
+            <Route path="text-generator" element={<TextGeneration />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="explore" element={<Explore />} />
+            <Route path="marketplace" element={<Marketplace />} />
+          </Routes>
+        </div>
       </div>
+      <AppFooter />
     </div>
   );
 };
