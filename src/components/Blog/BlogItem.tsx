638b431402a0902c4f64d7178029c90d33103117
usafzai <miyamotohiro.contact@gmail.com>
2024-02-02 09:19:29 -0500

    update news section

--- src/components/Blog/BlogItem.tsx
+++ src/components/Blog/BlogItem.tsx
@@ -1,37 +1,46 @@
 import { Link } from "react-router-dom";
 
-interface BlogItemProps {
+interface Post {
   hashtag?: string;
   title?: string;
   content?: string; // You can specify the correct type for content here
   src?: string;
+  featured_image?: string;
+  summary?: string;
+  slug?: string;
 }
 
-const BlogItem: React.FC<BlogItemProps> = ({
-  src,
-  hashtag,
-  title,
-  content,
-}) => {
+interface BlogItemProps {
+  post: Post;
+}
+
+const BlogItem: React.FC<BlogItemProps> = ({ post }) => {
   return (
     <>
       <article className="flex flex-col items-start w-full overflow-hidden">
-        <Link to="" className="w-full">
+        <Link to={`/news/${post.slug}`} className="w-full">
           <div className="overflow-hidden rounded-[6px] w-full max-w-full">
             <img
-              src={src || "https://cdn.buttercms.com/f9vWMDrTRPWCi7LWim2w"}
+              src={
+                post.featured_image ||
+                "https://cdn.buttercms.com/f9vWMDrTRPWCi7LWim2w"
+              }
               alt="blog"
-              className="w-[360px] h-[225px] overflow-hidden object-cover aspect-custom-16-10"
+              className="w-[360px] h-[225px] sm:w-full overflow-hidden object-cover aspect-custom-16-10"
             ></img>
           </div>
           <div className="py-[17px] px-0 max-w-full flex flex-col w-full items-start">
             <div className="text-[#6b66ff] text-[14px] font-semibold leading-[20px] pb-[5px]">
-              {hashtag}
+              {post.slug}
             </div>
             <header>
-              <h3 className=" font-normal leading-8 text-[24px]">{title}</h3>
+              <h3 className=" font-normal leading-8 text-[24px]">
+                {post.title}
+              </h3>
             </header>
-            <div className="leading-[1.4] text-[#d1d4e2] pt-5">{content}</div>
+            <div className="leading-[1.4] text-[#d1d4e2] pt-5">
+              {post.summary}
+            </div>
           </div>
         </Link>
       </article>
