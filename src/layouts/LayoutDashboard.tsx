a4077e3c50681a390312ac33d6e09a1706356f1a
usafzai <miyamotohiro.contact@gmail.com>
2024-02-04 19:50:01 -0500

    implement mobile responsive on main UI

--- src/layouts/LayoutDashboard.tsx
+++ src/layouts/LayoutDashboard.tsx
@@ -4,9 +4,9 @@ import { BGStyle } from "../assets";
 
 const LayoutDashboard = () => {
   return (
-    <div className="h-screen w-full bg-black p-8 pt-24 font-chakra relative overflow-hidden">
+    <div className="h-screen w-full md:h-full sm:h-full bg-black p-8 pt-24 font-chakra relative overflow-hidden">
       <div className="w-full h-full rounded-lg">
-        <div className="w-full flex flex-row h-full mx-auto min-w-[1000px] relative items-center justify-evenly">
+        <div className="w-full flex flex-row h-full mx-auto min-w-[1000px] md:min-w-full sm:min-w-full relative items-center justify-evenly md:flex-col sm:flex-col gap-5 md:gap-10 sm:gap-12">
           <div className="w-auto flex items-center justify-center">
             <div className="flex flex-col">
               <span className="text-white text-left font-semibold text-[30px] z-10 lg:text-[26px] md:text-[22px]">
@@ -19,41 +19,41 @@ const LayoutDashboard = () => {
                   Join now
                 </Link>
               </span>
-              <div className="flex flex-row items-center gap-5 pt-14">
-                <div className=" border-[4px] rounded-full border-[#9013ce] p-3">
-                  <div className="bg-[#9013ce] text-white rounded-full p-3 w-20 h-20 flex flex-col items-center justify-center">
-                    <span className="text-[12px]">Generate</span>
-                    <span className="text-[12px]">AI</span>
+              <div className="flex flex-row items-center gap-5 pt-14 text-[12px] sm:text-[10px] sm:justify-center">
+                <div className="border-[4px] rounded-full border-[#9013ce] p-3 sm:p-[6px]">
+                  <div className="bg-[#9013ce] text-white rounded-full p-3 w-20 h-20 sm:w-16 sm:h-16 flex flex-col items-center justify-center">
+                    <span className="">Generate</span>
+                    <span className="">AI</span>
                   </div>
                 </div>
                 <Icon
                   icon="ic:baseline-double-arrow"
-                  className="w-12 h-12 anim-color"
+                  className="w-12 h-12 anim-color sm:w-8 sm:h-8"
                 />
-                <div className=" border-[4px] rounded-full border-[#9013ce] p-3">
-                  <div className="bg-[#9013ce] text-white rounded-full p-3 w-20 h-20 flex flex-col items-center justify-center">
-                    <span className="text-[12px]">Minting</span>
-                    <span className="text-[12px]">NFT</span>
+                <div className=" border-[4px] rounded-full border-[#9013ce] p-3 sm:p-[6px]">
+                  <div className="bg-[#9013ce] text-white rounded-full p-3 w-20 h-20 sm:w-16 sm:h-16 flex flex-col items-center justify-center">
+                    <span className="">Minting</span>
+                    <span className="">NFT</span>
                   </div>
                 </div>
                 <Icon
                   icon="ic:baseline-double-arrow"
-                  className="w-12 h-12 anim-color animate-delay-200"
+                  className="w-12 h-12 anim-color animate-delay-200 sm:w-8 sm:h-8"
                 />
-                <div className=" border-[4px] rounded-full border-[#9013ce] p-3">
-                  <div className="bg-[#9013ce] text-white rounded-full p-3 w-20 h-20 flex flex-col items-center justify-center">
-                    <span className="text-[12px]">Mining</span>
-                    <span className="text-[12px]">Reward</span>
+                <div className=" border-[4px] rounded-full border-[#9013ce] p-3 sm:p-[6px]">
+                  <div className="bg-[#9013ce] text-white rounded-full p-3 w-20 h-20 sm:w-16 sm:h-16 flex flex-col items-center justify-center">
+                    <span className="">Mining</span>
+                    <span className="">Reward</span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="w-auto pl-10">
+          <div className="w-auto">
             <img
               src="./assets/characters/lion.jpg"
               alt="lion"
-              className="rounded-lg h-[750px] w-[560px]"
+              className="rounded-lg h-[750px] w-[560px] sm:w-[480px] sm:h-auto"
             />
           </div>
         </div>
