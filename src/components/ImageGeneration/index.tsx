fd5a74f1d70be4b463460cd941ba53b140a1184b
usafzai <miyamotohiro.contact@gmail.com>
2024-02-19 05:53:48 -0500

    update starkstyle & recent works

--- src/components/ImageGeneration/index.tsx
+++ src/components/ImageGeneration/index.tsx
@@ -40,7 +40,7 @@ const socket = io(process.env.REACT_APP_SOCKET_API || "http://localhost:5001");
 const userSelectedModelItem: ModelItem = {
   id: "1e60896f-3c26-4296-8ecc-53e2afecc132",
   modelType: "Finetuned Model",
-  label: "StarkAI Diffusion XL",
+  label: "StarkAI Xl v1",
   subLabel: "Alchemy V2",
   imgURI:
     "https://cdn.leonardo.ai/users/384ab5c8-55d8-47a1-be22-6a274913c324/generations/9ea08719-5fd1-4df7-9adc-5218637cba17/Leonardo_Diffusion_XL_a_brain_suspended_in_midair_bathed_in_a_1.jpg",
