02c308dcf7006fbe99088da3b736466730ee84bd
usafzai <miyamotohiro.contact@gmail.com>
2024-03-21 23:55:36 -0400

    update layout

--- src/components/Dashboard/LatestInnovation.tsx
+++ src/components/Dashboard/LatestInnovation.tsx
@@ -1,5 +1,3 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
 import Marquee from "react-fast-marquee";
 import { LeftBGStyle } from "../../assets";
 
